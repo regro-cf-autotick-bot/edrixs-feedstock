e489b5f1cf6b31442f6ee9e5fd6437f6b9e89603
thomashopkins32 <thomashopkins000@gmail.com>
2025-01-10 16:31:07 -0500

    [skip ci] add TODO for rsh install

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,6 +12,7 @@
 # build Fortran extension via f2py
 # $PYTHON setup.py build_ext --library-dirs=$LIBRARY_LIB
 
+# TODO: Solve this rsh install properly with EPEL, probably have to update docker images
 curl --output /tmp/rsh-0.17-102.el9.x86_64.rpm https://dl.fedoraproject.org/pub/epel/9/Everything/x86_64/Packages/r/rsh-0.17-102.el9.x86_64.rpm
 sudo yum install /tmp/rsh-0.17-102.el9.x86_64.rpm -y
 
